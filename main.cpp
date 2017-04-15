14410ab83ea2885bdd940fc091dde61a25972468
SteelPh0enix <phoenixpl512@gmail.com>
2017-04-15 16:01:32 +0200

    2fancy4me

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 #include <QApplication>
 #include <QFile>
 #include <QTextStream>
+#include "xyz.hpp"
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
@@ -13,6 +15,19 @@ int main(int argc, char *argv[])
         QTextStream ts(&f);
         a.setStyleSheet(ts.readAll());
     }
+
+    //standard c-tor
+    XYZ<int> test(1, 2, 3);
+    qDebug() << test.x << test.y << test.z << '\n';
+
+    //copy c-tor
+    XYZ<int> test2(test);
+    qDebug() << test2.x << test2.y << test2.z << '\n';
+
+    //casting
+    XYZ<float> test_f = static_cast<XYZ<float>>(test);
+    test_f.x += 3.14;
+    qDebug() << test_f.x << test_f.y << test_f.z << '\n';
 
     MainWindow w;
     w.show();
--- xyz.hpp
+++ xyz.hpp
@@ -5,40 +5,14 @@
 #include <string>
 
 template<typename T>
-class XYZ
-{
-    T m_x, m_y, m_z;
-public:
+struct XYZ {
+    T x, y, z;
+
     XYZ() {}
-    XYZ(T x, T y, T z) : m_x(x), m_y(y), m_z(z) {}
-    XYZ(const XYZ<T>& other) : m_x(other.getX()), m_y(other.getY()), m_z(other.getZ()) {}
+    XYZ(T arg_x, T arg_y, T arg_z) : x(arg_x), y(arg_y), z(arg_z) {}
+    XYZ(const XYZ<T>& other) : x(other.x), y(other.y), z(other.z) {}
     template<typename N>
-    XYZ(const XYZ<N>& other) : m_x(static_cast<N>(other.getX())), m_y(static_cast<N>(other.getY())), m_z(static_cast<N>(other.getZ())) {}
-
-    const T getX() const { return m_x; }
-    const T getY() const { return m_y; }
-    const T getZ() const { return m_z; }
-
-    void setX(T x) { m_x = x; }
-    void setY(T y) { m_y = y; }
-    void setZ(T z) { m_z = z; }
-
-    T& operator [](char key) {
-        switch(key) {
-        case 'x' : return m_x;
-            break;
-        case 'y' : return m_y;
-            break;
-        case 'z' : return m_z;
-            break;
-        default:
-            throw std::invalid_argument(key + std::string(" - invalid argument for XYZ<T>[]"));
-        }
-    }
-
-    const T& operator [](char key) const {
-        return const_cast<XYZ<T>*>(this)->operator [](key);
-    }
+    XYZ(const XYZ<N>& other) : x(static_cast<N>(other.x)), y(static_cast<N>(other.y)), z(static_cast<N>(other.z)) {}
 };
 
 #endif // XYZ_HPP
